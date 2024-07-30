67e575d37bdb9f5745748bb14ce5c9f0e5157724
cybergone <muhammadakmal.abdillah89@gmail.com>
2024-07-30 16:51:04 +0700

    update

--- WINDOWS-AUDIT .ps1
+++ WINDOWS-AUDIT .ps1
@@ -4,14 +4,14 @@ _ _ _ _ _  _ ___  ____ _ _ _ ____    ____ _  _ ___  _ ___
 | | | | |\ | |  \ |  | | | | [__     |__| |  | |  \ |  |  
 |_|_| | | \| |__/ |__| |_|_| ___]    |  | |__| |__/ |  |
              
-                      @cybergone
+                                              ~cybergone
 
 "
 #Membuat direktori pada "C:\temp\Windows Audit"
 
-$Direktori = "C:\temp\Windows Audit"
-if (!(Test-Path $Direktori)) {
-    New-Item -ItemType Directory -Path $Direktori
+$Directory = "C:\temp\Windows Audit"
+if (!(Test-Path $Directory)) {
+    New-Item -ItemType Directory -Path $Directory
 }
 
 $hostname = hostname
@@ -19,43 +19,38 @@ $hostname = hostname
 ######### Computer Information #########
 
 Write-Host "Getting Systeminfo..."
-systeminfo > "$Direktori\systeminfo.txt"
+systeminfo > "$Directory\systeminfo.txt"
 
 Write-Host "Getting Privilege Information..."
-whoami /priv > "$Direktori\whoami-priv.txt"
+whoami /priv > "$Directory\whoami-priv.txt"
 
 Write-Host "Getting IP Configuration..."
-ipconfig > "$Direktori\ipconfig.txt"
+ipconfig > "$Directory\ipconfig.txt"
 
 Write-Host "Getting PowerShell version..."
-$PSVersionTable.PSVersion > "$Direktori\PowerShell version.txt"
+$PSVersionTable.PSVersion > "$Directory\PowerShell version.txt"
 
 Write-Host "Getting PC Timezone..."
-Get-TimeZone > "$Direktori\Timezone.txt"
+Get-TimeZone > "$Directory\Timezone.txt"
 
 Write-Host "Getting Installed Programs..."
-Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Select-Object InstallDate, DisplayName, DisplayVersion, Publisher, InstallLocation, InstallSource | Sort-Object DisplayName > "$Direktori\InstalledPrograms.txt"
+Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Select-Object InstallDate, DisplayName, DisplayVersion, Publisher, InstallLocation, InstallSource | Sort-Object DisplayName > "$Directory\InstalledPrograms.txt"
+
+Write-Host "Getting HotFix/Update Information..."
+wmic qfe get Caption,Description,HotfixID,InstalledOn,InstalledBy > "$Directory\Get-Hotfix.txt"
 
 Write-Host "Getting Aliases for"$hostname"..."
-net localgroup > "$Direktori\net localgroup $hostname.txt"
+net localgroup > "$Directory\net localgroup $hostname.txt"
 
 Write-Host "Getting PC Antivirus Information..."
-Get-WmiObject -Namespace root\SecurityCenter2 -Class AntiVirusProduct > "$Direktori\Antivirus Installed.txt"
+Get-WmiObject -Namespace root\SecurityCenter2 -Class AntiVirusProduct > "$Directory\Antivirus Installed.txt"
 
 Write-Host "Getting PC Last Boot Up Time..."
-Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object LastBootUpTime > "$Direktori\Last BootUp Time.txt"
+Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object LastBootUpTime > "$Directory\Last BootUp Time.txt"
 
 Write-Host "Getting PC Scheduled Tasks..."
-schtasks /Query /FO TABLE > "$Direktori\schtask.txt"
+schtasks /Query /FO TABLE > "$Directory\schtask.txt"
 
-
-$icaclsdir = "C:\Windows\System32"
-Write-Host "Getting permissions for exe files in C:\Windows\system32..."
-Get-ChildItem -Path $icaclsdir -Filter "*.exe" | ForEach-Object {
-    $icaclspath = $_.FullName
-    icacls $icaclspath
-    
-} | Out-File "C:\temp\Windows Audit\icacls.txt"
 
 
 
@@ -74,10 +69,31 @@ else{
     Write-Host "SMBv1 Not Found"
 }
 
+Write-Host "Checking SMBv2..."
+$cekSMB1 = (Get-SmbServerConfiguration).EnableSMB2Protocol
+
+if ($cekSMB1 -eq $false){
+    Write-Host "SMBv2 Disabled" -ForegroundColor "Red"
+}
+elseif ($cekSMB1 -eq $true) {
+    Write-Host "SMBv2 Enabled" -ForegroundColor "Green"
+}
+else{
+    Write-Host "SMBv2 Not Found"
+}
+
 
 
 ######### Checking Possible Privilege Escalation
 Write-Host "Checking Possible Privilege Escalations...."
+
+Write-Host "Getting permissions for every exe files in C:\Windows\system32..."
+$icaclsdir = "C:\Windows\System32"
+Get-ChildItem -Path $icaclsdir -Filter "*.exe" | ForEach-Object {
+    $icaclspath = $_.FullName
+    icacls $icaclspath
+    
+} | Out-File "C:\temp\Windows Audit\icacls.txt"
 
 Write-Host "Checking Unquoted Service Path..."
 
